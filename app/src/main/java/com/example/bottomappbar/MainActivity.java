0c3433e23f206716ad2e142ec93431ff691a01f9
Luozhang <674818402@qq.com>
2019-05-27 14:36:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/bottomappbar/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.bottomappbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+    FloatingActionButton floatingActionButton;
+    BottomAppBar bottomAppBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        floatingActionButton = findViewById(R.id.fab);
+        bottomAppBar = findViewById(R.id.bottomAppBar);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("WrongConstant")
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view,"Fab is clicked", Snackbar.LENGTH_LONG)
+                        .setAction("UNDO", null)
+                        .show();
+            }
+        });
+    }
+}
